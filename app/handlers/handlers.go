567a7403a6a6f6a73be5792125ca46708b65d1c5
Joël Foramitti <57440945+JoelForamitti@users.noreply.github.com>
2023-05-08 13:34:30 +0200

    Docker setup

---
+++ app/handlers/handlers.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"augustin/database"
+	"augustin/models"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/utils"
+)
+
+// Return hello world
+func HelloWorld(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"detail": "Hello, World!",
+	})
+}
+
+
+// UserList returns a list of users
+func UserList(c *fiber.Ctx) error {
+	users := database.Get()
+
+	return c.JSON(fiber.Map{
+		"success": true,
+		"users":   users,
+	})
+}
+
+// UserCreate registers a user
+func UserCreate(c *fiber.Ctx) error {
+	user := &models.User{
+		// Note: when writing to external database,
+		// we can simply use - Name: c.FormValue("user")
+		Name: utils.CopyString(c.FormValue("user")),
+	}
+	database.Insert(user)
+
+	return c.JSON(fiber.Map{
+		"success": true,
+		"user":    user,
+	})
+}
+
+// NotFound returns custom 404 page
+func NotFound(c *fiber.Ctx) error {
+	return c.Status(404).SendFile("./static/private/404.html")
+}
