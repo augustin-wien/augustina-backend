4a2a5b7942b5053d3179268dac46d25e715ee39b
Leon <l.beccard@protonmail.com>
2023-05-22 17:06:14 +0200

    Add static file server for images and 2 api calls

--- app/main.go
+++ app/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"augustin/database"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	log "github.com/sirupsen/logrus"
-
-	"augustin/database"
 )
+
+type setting struct {
+	Color string  `json:"color"`
+	Logo  string  `json:"logo"`
+	Price float64 `json:"price"`
+}
+
+type vendor struct {
+	Credit   float64 `json:"credit"`
+	QRcode   string  `json:"qrcode"`
+	IDnumber string  `json:"id-number"`
+}
 
 func initLog() {
 	customFormatter := new(log.TextFormatter)
@@ -37,6 +50,24 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(greeting))
 }
 
+func Setting(w http.ResponseWriter, r *http.Request) {
+	marshal_struct, err := json.Marshal(setting{Color: "red", Logo: "/img/Augustin-Logo-Rechteck.jpg", Price: 3.14})
+	if err != nil {
+		log.Info(err)
+		return
+	}
+	w.Write([]byte(marshal_struct))
+}
+
+func Vendor(w http.ResponseWriter, r *http.Request) {
+	marshal_struct, err := json.Marshal(vendor{Credit: 1.61, QRcode: "/img/Augustin-QR-Code.png", IDnumber: "123456789"})
+	if err != nil {
+		log.Info(err)
+		return
+	}
+	w.Write([]byte(marshal_struct))
+}
+
 type Server struct {
 	Router *chi.Mux
 	// Db, config can be added here
@@ -54,5 +85,12 @@ func (s *Server) MountHandlers() {
 
 	// Mount all handlers here
 	s.Router.Get("/hello", HelloWorld)
+
+	s.Router.Get("/settings", Setting)
+
+	s.Router.Get("/vendor", Vendor)
+
+	fs := http.FileServer(http.Dir("img"))
+	s.Router.Handle("/img/*", http.StripPrefix("/img/", fs))
 
 }
