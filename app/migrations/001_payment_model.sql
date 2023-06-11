fb3473b21b7564c884b6dd75923749d571d6f238
Joël Foramitti <57440945+JoelForamitti@users.noreply.github.com>
2023-06-11 16:43:20 +0200

    Require all fields

--- app/migrations/001_payment_model.sql
+++ app/migrations/001_payment_model.sql
@@ -8,13 +8,12 @@ CREATE TABLE PaymentTypes (
     Name varchar(255) NOT NULL
 );
 
--- TODO: All NOT NULL
 CREATE TABLE Payments (
     ID bigserial PRIMARY KEY,
     Timestamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    Sender integer REFERENCES Accounts,
-    Receiver integer REFERENCES Accounts,
-    Type integer REFERENCES PaymentTypes,
+    Sender integer NOT NULL REFERENCES Accounts,
+    Receiver integer NOT NULL REFERENCES Accounts,
+    Type integer NOT NULL REFERENCES PaymentTypes,
     Amount real NOT NULL
 );
 
