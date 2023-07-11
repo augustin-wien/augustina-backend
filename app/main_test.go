ed7e0fcb5c326ec5a7978fc0a2e6e434b67b55c5
Leon <l.beccard@protonmail.com>
2023-07-11 17:15:25 +0200

    Remove and comment code from main and main_test to shorten files

--- app/main_test.go
+++ app/main_test.go
@@ -2,22 +2,12 @@
 package main
 
 import (
-	"augustin/database"
-	"augustin/keycloak"
-	"augustin/structs"
-	"bytes"
-	"context"
-	"encoding/json"
+	"augustin/handlers"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 
 	"github.com/Nerzal/gocloak/v13"
-	"github.com/jackc/pgx/v5/pgtype"
-	"github.com/joho/godotenv"
-	log "github.com/sirupsen/logrus"
-	"github.com/stretchr/testify/require"
 )
 
 // WARNING: The tests use the main database, do not run tests in production
@@ -26,7 +16,7 @@ import (
 // then executes the request by calling ServeHTTP in the router
 // after which the handler writes the response to the response recorder
 // which we can then inspect.
-func executeRequest(req *http.Request, s *Server) *httptest.ResponseRecorder {
+func executeRequest(req *http.Request, s *handlers.Server) *httptest.ResponseRecorder {
 	rr := httptest.NewRecorder()
 	s.Router.ServeHTTP(rr, req)
 
@@ -41,30 +31,6 @@ func checkResponseCode(t *testing.T, expected, actual int) {
 	}
 }
 
-func TestHelloWorld(t *testing.T) {
-	godotenv.Load("../.env")
-	// Initialize database
-	database.InitDb()
-
-	// Create a New Server Struct
-	s := CreateNewServer()
-
-	// Mount Handlers
-	s.MountHandlers()
-
-	// Create a New Request
-	req, _ := http.NewRequest("GET", "/api/hello/", nil)
-
-	// Execute Request
-	response := executeRequest(req, s)
-
-	// Check the response code
-	checkResponseCode(t, http.StatusOK, response.Code)
-
-	// We can use testify/require to assert values, as it is more convenient
-	require.Equal(t, "Hello, world!", response.Body.String())
-}
-
 func lookupRole(roleName string, roles []*gocloak.Role) *gocloak.Role {
 	for _, role := range roles {
 		if *role.Name == roleName {
@@ -74,240 +40,92 @@ func lookupRole(roleName string, roles []*gocloak.Role) *gocloak.Role {
 	return nil
 }
 
-func TestKeycloak(t *testing.T) {
-	godotenv.Load("../.env")
-	// Test the keycloak functions
-	keycloak.InitializeOauthServer()
-	var err error
-	role_name := "testrole"
-	err = keycloak.KeycloakClient.CreateRole(role_name)
-	if err != nil {
-		log.Error("Create role failed:", err)
-	}
-	role, err := keycloak.KeycloakClient.GetRole(role_name)
-	if err != nil {
-		log.Error("Get role failed:", err)
-	}
+// func TestKeycloak(t *testing.T) {
+// 	godotenv.Load("../.env")
+// 	// Test the keycloak functions
+// 	keycloak.InitializeOauthServer()
+// 	var err error
+// 	role_name := "testrole"
+// 	err = keycloak.KeycloakClient.CreateRole(role_name)
+// 	if err != nil {
+// 		log.Error("Create role failed:", err)
+// 	}
+// 	role, err := keycloak.KeycloakClient.GetRole(role_name)
+// 	if err != nil {
+// 		log.Error("Get role failed:", err)
+// 	}
 
-	require.Equal(t, role_name, *role.Name)
+// 	require.Equal(t, role_name, *role.Name)
 
-	_, err = keycloak.KeycloakClient.CreateUser("testuser", "testuser", "testuser@example.com")
-	if err != nil {
-		log.Error("Create user failed:", err)
-	}
+// 	_, err = keycloak.KeycloakClient.CreateUser("testuser", "testuser", "testuser@example.com")
+// 	if err != nil {
+// 		log.Error("Create user failed:", err)
+// 	}
 
-	user, err := keycloak.KeycloakClient.GetUser("testuser@example.com")
-	if err != nil {
-		log.Error("Get user failed:", err)
-		panic(err)
-	}
+// 	user, err := keycloak.KeycloakClient.GetUser("testuser@example.com")
+// 	if err != nil {
+// 		log.Error("Get user failed:", err)
+// 		panic(err)
+// 	}
 
-	err = keycloak.KeycloakClient.AssignRole(*user.ID, role_name)
-	if err != nil {
-		log.Error("Assign role failed:", err)
-	}
+// 	err = keycloak.KeycloakClient.AssignRole(*user.ID, role_name)
+// 	if err != nil {
+// 		log.Error("Assign role failed:", err)
+// 	}
 
-	roles, err := keycloak.KeycloakClient.GetUserRoles(*user.ID)
-	if err != nil {
-		log.Error("Get user failed:", err)
-	}
-	require.NotNil(t, lookupRole(role_name, roles))
+// 	roles, err := keycloak.KeycloakClient.GetUserRoles(*user.ID)
+// 	if err != nil {
+// 		log.Error("Get user failed:", err)
+// 	}
+// 	require.NotNil(t, lookupRole(role_name, roles))
 
-	err = keycloak.KeycloakClient.UnassignRole(*user.ID, role_name)
-	if err != nil {
-		log.Error("Unassign role failed:", err)
-	}
+// 	err = keycloak.KeycloakClient.UnassignRole(*user.ID, role_name)
+// 	if err != nil {
+// 		log.Error("Unassign role failed:", err)
+// 	}
 
-	roles, err = keycloak.KeycloakClient.GetUserRoles(*user.ID)
-	if err != nil {
-		log.Error("Get user failed:", err)
-	}
-	require.Nil(t, lookupRole(role_name, roles))
+// 	roles, err = keycloak.KeycloakClient.GetUserRoles(*user.ID)
+// 	if err != nil {
+// 		log.Error("Get user failed:", err)
+// 	}
+// 	require.Nil(t, lookupRole(role_name, roles))
 
-	err = keycloak.KeycloakClient.DeleteUser(*user.ID)
-	if err != nil {
-		log.Error("Delete user failed:", err)
-	}
+// 	err = keycloak.KeycloakClient.DeleteUser(*user.ID)
+// 	if err != nil {
+// 		log.Error("Delete user failed:", err)
+// 	}
 
-	err = keycloak.KeycloakClient.DeleteRole(role_name)
-	if err != nil {
-		log.Error("Delete role failed:", err)
-	}
+// 	err = keycloak.KeycloakClient.DeleteRole(role_name)
+// 	if err != nil {
+// 		log.Error("Delete role failed:", err)
+// 	}
 
-}
+// }
 
-func TestHelloWorldAuth(t *testing.T) {
-	// Initialize database
-	// TODO: This is not a test database, but the real one!
-	err := godotenv.Load("../.env")
-	if err != nil {
-		log.Error("Error loading .env file")
-	}
-	database.InitDb()
+// func TestHelloWorldAuth(t *testing.T) {
+// 	// Initialize database
+// 	// TODO: This is not a test database, but the real one!
+// 	err := godotenv.Load("../.env")
+// 	if err != nil {
+// 		log.Error("Error loading .env file")
+// 	}
+// 	database.InitDb()
 
-	// Create a New Server Struct
-	s := CreateNewServer()
+// 	// Create a New Server Struct
+// 	s := handlers.CreateNewServer()
 
-	// Mount Handlers
-	s.MountHandlers()
+// 	// Mount Handlers
+// 	s.MountHandlers()
 
-	// Create a New Request
-	req, _ := http.NewRequest("GET", "/api/auth/hello/", nil)
+// 	// Create a New Request
+// 	req, _ := http.NewRequest("GET", "/api/auth/hello/", nil)
 
-	// Execute Request
-	response := executeRequest(req, s)
+// 	// Execute Request
+// 	response := executeRequest(req, s)
 
-	// Check the response code
-	checkResponseCode(t, 401, response.Code)
+// 	// Check the response code
+// 	checkResponseCode(t, 401, response.Code)
 
-	// We can use testify/require to assert values, as it is more convenient
-	require.Equal(t, "Unauthorized\n", response.Body.String())
-}
-
-func TestPayments(t *testing.T) {
-	godotenv.Load("../.env")
-	// Initialize test case
-	database.InitDb()
-	s := CreateNewServer()
-	s.MountHandlers()
-
-	// Reset tables
-	_, err := database.Db.Dbpool.Query(context.Background(), "truncate Payments, PaymentTypes, Accounts")
-	if err != nil {
-		log.Errorf("Truncate payments failed: %v\n", err)
-	}
-
-	// Set up a payment type
-	payment_type_id, err := database.Db.CreatePaymentType(
-		structs.PaymentType{Name: pgtype.Text{String: "Test type", Valid: true}},
-	)
-	if err != nil {
-		log.Errorf("CreatePaymentType failed: %v\n", err)
-	}
-
-	// Set up a payment account
-	account_id, err := database.Db.CreateAccount(
-		structs.Account{Name: pgtype.Text{String: "Test account", Valid: true}},
-	)
-	if err != nil {
-		log.Errorf("CreateAccount failed: %v\n", err)
-	}
-
-	// Create payments via API
-	f := structs.PaymentBatch{
-		Payments: []structs.Payment{
-			{
-				Sender:   account_id,
-				Receiver: account_id,
-				Type:     payment_type_id,
-				Amount:   pgtype.Float4{Float32: 3.14, Valid: true},
-			},
-		},
-	}
-	var body bytes.Buffer
-	err = json.NewEncoder(&body).Encode(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-	req, _ := http.NewRequest("POST", "/api/payments/", &body)
-	response := executeRequest(req, s)
-
-	// Check the response
-	checkResponseCode(t, http.StatusOK, response.Code)
-
-	// Get payments via API
-	req2, err := http.NewRequest("GET", "/api/payments/", nil)
-	response2 := executeRequest(req2, s)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Check the response
-	checkResponseCode(t, http.StatusOK, response2.Code)
-
-	// Unmarshal response
-	var payments []structs.Payment
-	err = json.Unmarshal(response2.Body.Bytes(), &payments)
-	if err != nil {
-		panic(err)
-	}
-
-	// Test payments response
-	require.Equal(t, payments[0].Amount.Float32, float32(3.14))
-	require.Equal(t, payments[0].Sender, account_id)
-	require.Equal(t, payments[0].Receiver, account_id)
-	require.Equal(t, payments[0].Type, payment_type_id)
-	require.Equal(t, payments[0].Timestamp.Time.Day(), time.Now().Day())
-	require.Equal(t, payments[0].Timestamp.Time.Hour(), time.Now().Hour())
-	require.Equal(t, len(payments), 1)
-
-}
-
-func TestSettings(t *testing.T) {
-	godotenv.Load("../.env")
-	database.InitDb()
-	s := CreateNewServer()
-	s.MountHandlers()
-
-	// Define settings
-	database.Db.UpdateSettings(structs.Settings{
-		Color: "red",
-		Logo:  "/img/Augustin-Logo-Rechteck.jpg",
-		Items: []structs.Item{
-			{
-				Name:  "calendar",
-				Price: 3.14,
-			},
-			{
-				Name:  "cards",
-				Price: 12.12,
-			},
-		},
-	},
-	)
-
-	// Create a New Request
-	req, _ := http.NewRequest("GET", "/api/settings/", nil)
-
-	// Execute Request
-	response := executeRequest(req, s)
-
-	// Check the response code
-	checkResponseCode(t, http.StatusOK, response.Code)
-
-	// Unmarshal response
-	var settings structs.Settings
-	err := json.Unmarshal(response.Body.Bytes(), &settings)
-	if err != nil {
-		panic(err)
-	}
-
-	// Test response
-	require.Equal(t, settings.Color, "red")
-	require.Equal(t, settings.Logo, "/img/Augustin-Logo-Rechteck.jpg")
-	require.Equal(t, settings.Items[0].Name, "calendar")
-	require.Equal(t, settings.Items[0].Price, float32(3.14))
-	require.Equal(t, settings.Items[1].Name, "cards")
-	require.Equal(t, settings.Items[1].Price, float32(12.12))
-}
-
-func TestVendor(t *testing.T) {
-	godotenv.Load("../.env")
-	// Create a New Server Struct
-	s := CreateNewServer()
-	// Mount Handlers
-	s.MountHandlers()
-
-	// Create a New Request
-	req, _ := http.NewRequest("GET", "/api/vendor/", nil)
-
-	// Execute Request
-	response := executeRequest(req, s)
-
-	// Check the response code
-	checkResponseCode(t, http.StatusOK, response.Code)
-	marshal_struct, _ := json.Marshal(&structs.Vendor{Credit: 1.61, QRcode: "/img/Augustin-QR-Code.png", IDnumber: "123456789"})
-
-	// We can use testify/require to assert values, as it is more convenient
-	require.Equal(t, string(marshal_struct), response.Body.String())
-}
+// 	// We can use testify/require to assert values, as it is more convenient
+// 	require.Equal(t, "Unauthorized\n", response.Body.String())
+// }
