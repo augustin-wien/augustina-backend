76ac8309ca10e766fbd9ca22657f85ade3f8bd23
Leon <l.beccard@protonmail.com>
2023-07-26 15:29:53 +0200

    Add authentication to VivaWallet

--- app/vivawallet/vivawallet.go
+++ app/vivawallet/vivawallet.go
@@ -1,47 +1,55 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	"bytes"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"time"
 )
 
-type Post struct {
-	Userid string `json:"userId"`
-	ID     int    `json:"id"`
-	Title  string `json:"title"`
-	Body   string `json:"body"`
+func main() {
+	apiURL := "https://demo-accounts.vivapayments.com"
+	resource := "/connect/token"
+	jsonPost := []byte(`grant_type=client_credentials`)
+	u, _ := url.ParseRequestURI(apiURL)
+	u.Path = resource
+	urlStr := u.String() // "https://demo-accounts.vivapayments.com/connect/token"
+
+	body, err := authenticateToVivaWallet(urlStr, jsonPost)
+	if err != nil {
+		log.Fatalf("Authentication failed: %s", err)
+	}
+	log.Printf("res body: %s", string(body))
+
 }
 
-func main() {
-	params := url.Values{}
-	params.Add("title", "foo")
-	params.Add("body", "bar")
-	params.Add("userId", "1")
-	fmt.Println(params.Encode())
-
-	resp, err := http.PostForm("https://jsonplaceholder.typicode.com/posts", params)
+func authenticateToVivaWallet(urlStr string, jsonPost []byte) ([]byte, error) {
+	req, err := http.NewRequest("POST", urlStr, bytes.NewReader(jsonPost))
 	if err != nil {
-		log.Printf("Request Failed: %s", err)
-		return
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body) // Log the request body
-	if err != nil {
-		log.Printf("Reading body failed: %s", err)
-		return
-	}
-	bodyString := string(body)
-	log.Print(bodyString) // Unmarshal result
-	post := Post{}
-	err = json.Unmarshal(body, &post)
-	if err != nil {
-		log.Printf("Reading body failed: %s", err)
-		return
+		log.Fatalf("Building request failed: %s", err)
 	}
 
-	log.Printf("Post added with ID %d", post.ID)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Authorization", "Basic ZTc2cnBldnR1cmZma3RuZTduMTh2MG94eWozbTZzNTMycjFxNHk0azR4eDEzLmFwcHMudml2YXBheW1lbnRzLmNvbTpxaDA4RmtVMGRGOHZNd0g3NmpHQXVCbVdpYjlXc1A=")
+
+	// Create a new client with a 10 second timeout
+	// do not forget to set timeout; otherwise, no timeout!
+	client := http.Client{Timeout: 10 * time.Second}
+	// send the request
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("impossible to send request: %s", err)
+	}
+	log.Printf("status Code: %d", res.StatusCode)
+
+	// closes the body after the function returns
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body) // Log the request body
+	if err != nil {
+		log.Printf("Reading body failed: %s", err)
+		return nil, err
+	}
+	return body, nil
 }
