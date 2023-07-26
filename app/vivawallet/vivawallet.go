e68b724b4ccbe316afe37911a79c5a68e8ede39a
Leon <l.beccard@protonmail.com>
2023-07-26 16:57:50 +0200

    Add first setup for creating PaymentOrder

--- app/vivawallet/vivawallet.go
+++ app/vivawallet/vivawallet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
@@ -9,15 +10,35 @@ import (
 	"time"
 )
 
-func main() {
-	apiURL := "https://demo-accounts.vivapayments.com"
-	resource := "/connect/token"
-	jsonPost := []byte(`grant_type=client_credentials`)
-	u, _ := url.ParseRequestURI(apiURL)
-	u.Path = resource
-	urlStr := u.String() // "https://demo-accounts.vivapayments.com/connect/token"
+type PaymentOrder struct {
+	Amount              int      `json:"amount"`
+	customerTrns        string   `json:"customerTrns"`
+	customer            Customer `json:"customer"`
+	paymentTimeout      int      `json:"paymentTimeout"`
+	preauth             bool     `json:"preauth"`
+	allowRecurring      bool     `json:"allowRecurring"`
+	maxInstallments     int      `json:"maxInstallments"`
+	paymentNotification bool     `json:"paymentNotification"`
+	tipAmount           int      `json:"tipAmount"`
+	disableExactAmount  bool     `json:"disableExactAmount"`
+	disableCash         bool     `json:"disableCash"`
+	disableWallet       bool     `json:"disableWallet"`
+	sourceCode          string   `json:"sourceCode"`
+	merchantTrns        string   `json:"merchantTrns"`
+	tags                []string `json:"tags"`
+}
 
-	body, err := authenticateToVivaWallet(urlStr, jsonPost)
+type Customer struct {
+	Email       string `json:"email"`
+	Fullname    string `json:"fullName"`
+	Phone       string `json:"phone"`
+	countryCode string `json:"countryCode"`
+	requestLang string `json:"requestLang"`
+}
+
+func main() {
+
+	body, err := authenticateToVivaWallet()
 	if err != nil {
 		log.Fatalf("Authentication failed: %s", err)
 	}
@@ -25,7 +46,14 @@ func main() {
 
 }
 
-func authenticateToVivaWallet(urlStr string, jsonPost []byte) ([]byte, error) {
+func authenticateToVivaWallet() ([]byte, error) {
+	apiURL := "https://demo-accounts.vivapayments.com"
+	resource := "/connect/token"
+	jsonPost := []byte(`grant_type=client_credentials`)
+	u, _ := url.ParseRequestURI(apiURL)
+	u.Path = resource
+	urlStr := u.String() // "https://demo-accounts.vivapayments.com/connect/token"
+
 	req, err := http.NewRequest("POST", urlStr, bytes.NewReader(jsonPost))
 	if err != nil {
 		log.Fatalf("Building request failed: %s", err)
@@ -52,4 +80,49 @@ func authenticateToVivaWallet(urlStr string, jsonPost []byte) ([]byte, error) {
 		return nil, err
 	}
 	return body, nil
+}
+
+func createPaymentOrder(accessToken string) (orderCode string) {
+	apiURL := "https://demo-api.vivapayments.com"
+	resource := "/checkout/v2/orders"
+	u, _ := url.ParseRequestURI(apiURL)
+	u.Path = resource
+	urlStr := u.String() // "https://demo-accounts.vivapayments.com/connect/token"
+
+	customer := Customer{
+		Email:       "test@example.com",
+		Fullname:    "Mira Mendel",
+		Phone:       "1234567890",
+		countryCode: "GR",
+		requestLang: "en-GB",
+	}
+
+	paymentOrder := PaymentOrder{
+		Amount:              1000,
+		customerTrns:        "testCustomerTrns",
+		customer:            customer,
+		paymentTimeout:      300,
+		preauth:             false,
+		allowRecurring:      false,
+		maxInstallments:     0,
+		paymentNotification: true,
+		tipAmount:           100,
+		disableExactAmount:  false,
+		disableCash:         true,
+		disableWallet:       true,
+		sourceCode:          "6343",
+		merchantTrns:        "testMerchantTrns",
+		tags:                []string{"testTag1", "testTag2"},
+	}
+
+	jsonPost, err := json.Marshal(paymentOrder)
+
+	req, err := http.NewRequest("POST", urlStr, bytes.NewReader(jsonPost))
+	if err != nil {
+		log.Fatalf("Building request failed: %s", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+
 }
