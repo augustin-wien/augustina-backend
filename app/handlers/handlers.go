7692c24778dea1d3ed3485eb0f1a04c910a159fa
Leon <l.beccard@protonmail.com>
2023-09-01 16:05:34 +0200

    Add logging for debugging purposes

--- app/handlers/handlers.go
+++ app/handlers/handlers.go
@@ -328,9 +328,9 @@ func DeleteItem(w http.ResponseWriter, r *http.Request) {
 // Orders ---------------------------------------------------------------------
 
 type CreateOrderRequest struct {
-	Entries  []database.OrderEntry
-	User     string
-	Vendor   int32
+	Entries []database.OrderEntry
+	User    string
+	Vendor  int32
 }
 
 type CreateOrderResponse struct {
@@ -456,6 +456,7 @@ func VerifyPaymentOrder(w http.ResponseWriter, r *http.Request) {
 	// Get transaction ID from URL parameter
 	OrderCode := r.URL.Query().Get("s")
 	TransactionID := r.URL.Query().Get("t")
+	log.Info("Receiving order code: ", OrderCode, "and transaction ID: ", TransactionID)
 
 	// Get payment order from database
 	order, err := database.Db.GetOrderByOrderCode(OrderCode)
