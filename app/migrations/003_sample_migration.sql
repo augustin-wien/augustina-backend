a9398b60facf3b16dea3a4392324755a13c27add
Joël Foramitti <57440945+jofmi@users.noreply.github.com>
2023-09-27 09:47:46 +0200

    Revert "Try to redefine truncate function"
    This reverts commit 26105a6b7762cc2a14e7ec6376c527fe881a4f90.

--- app/migrations/003_sample_migration.sql
+++ app/migrations/003_sample_migration.sql
@@ -4,18 +4,6 @@
 ALTER TABLE Settings
 ADD COLUMN MaxOrderAmount integer DEFAULT 10000;
 
-CREATE OR REPLACE FUNCTION truncate_tables(username IN VARCHAR) RETURNS void AS $$
-DECLARE
-    statements CURSOR FOR
-        SELECT tablename FROM pg_tables
-        WHERE tableowner = username AND schemaname = 'public' AND tablename != 'schema_version';
-BEGIN
-    FOR stmt IN statements LOOP
-        EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;';
-    END LOOP;
-END;
-$$ LANGUAGE plpgsql;
-
 ---- create above / drop below ----
 
 ALTER TABLE Settings
