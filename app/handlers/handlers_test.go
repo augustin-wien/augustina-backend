5320ab712db97b09736105607551f26694275365
Leon <l.beccard@protonmail.com>
2023-09-28 15:44:42 +0200

    Fix testing error

--- app/handlers/handlers_test.go
+++ app/handlers/handlers_test.go
@@ -146,8 +146,8 @@ func TestItems(t *testing.T) {
 	var resItems []database.Item
 	err := json.Unmarshal(res.Body.Bytes(), &resItems)
 	utils.CheckError(t, err)
-	require.Equal(t, 2, len(resItems))
-	require.Equal(t, "Test item", resItems[1].Name)
+	require.Equal(t, 4, len(resItems))
+	require.Equal(t, "Test item", resItems[3].Name)
 
 	// Update (multipart form!)
 	body := new(bytes.Buffer)
@@ -164,17 +164,17 @@ func TestItems(t *testing.T) {
 	res = utils.TestRequest(t, r, "GET", "/api/items/", nil, 200)
 	err = json.Unmarshal(res.Body.Bytes(), &resItems)
 	utils.CheckError(t, err)
-	require.Equal(t, 2, len(resItems))
-	require.Equal(t, "Updated item name", resItems[1].Name)
-	require.Contains(t, resItems[1].Image, "test")
-	require.Contains(t, resItems[1].Image, ".jpg")
+	require.Equal(t, 4, len(resItems))
+	require.Equal(t, "Updated item name", resItems[3].Name)
+	require.Contains(t, resItems[3].Image, "test")
+	require.Contains(t, resItems[3].Image, ".jpg")
 
 	// Check file
 	dir, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
-	file, err := os.ReadFile(dir + "/" + resItems[1].Image)
+	file, err := os.ReadFile(dir + "/" + resItems[3].Image)
 	utils.CheckError(t, err)
 	require.Equal(t, `i am the content of a jpg file :D`, string(file))
 
@@ -190,9 +190,9 @@ func TestItems(t *testing.T) {
 	res = utils.TestRequest(t, r, "GET", "/api/items/", nil, 200)
 	err = json.Unmarshal(res.Body.Bytes(), &resItems)
 	utils.CheckError(t, err)
-	require.Equal(t, 2, len(resItems))
-	require.Equal(t, "Updated item name 2", resItems[1].Name)
-	require.Equal(t, resItems[1].Image, "Test")
+	require.Equal(t, 4, len(resItems))
+	require.Equal(t, "Updated item name 2", resItems[3].Name)
+	require.Equal(t, resItems[3].Image, "Test")
 
 }
 
