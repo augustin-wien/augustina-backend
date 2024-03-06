708d405e8c8cb89e9440213d8b06aa3dddff62d4
Leon <l.beccard@protonmail.com>
2024-03-06 17:56:33 +0100

    Add migration file

---
+++ app/migrations/006_add_PDF_table.sql
@@ -0,0 +1,10 @@
+-- Write your migrate up statements here
+CREATE TABLE PDF (
+    ID serial PRIMARY KEY,
+    Path varchar(255) UNIQUE NOT NULL DEFAULT ''
+);
+---- create above / drop below ----
+DROP TABLE IF EXISTS PDF;
+
+-- Write your migrate down statements here. If this migration is irreversible
+-- Then delete the separator line above.
