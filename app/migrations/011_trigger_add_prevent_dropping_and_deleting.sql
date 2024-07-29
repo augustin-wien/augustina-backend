eada45f8fc707ec09d642652bad4b753dd3af26a
Leon <l.beccard@protonmail.com>
2024-07-29 16:29:01 +0200

    Feat/Finalize migration file

--- app/migrations/011_trigger_add_prevent_dropping_and_deleting.sql
+++ app/migrations/011_trigger_add_prevent_dropping_and_deleting.sql
@@ -13,62 +13,161 @@ BEFORE DELETE ON Vendor
 FOR EACH ROW
 EXECUTE FUNCTION prevent_delete_vendor();
 
+-- Same for Accounttype, Account, Item, PaymentOrder, OrderEntry, Payment, Settings, DBsettings, PDF, PDFDownloads
+CREATE OR REPLACE FUNCTION prevent_delete_accounttype()
+RETURNS trigger AS $$
+BEGIN
+    RAISE EXCEPTION 'Cannot delete from table AccountType';
+    -- This will prevent the delete operation
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
 
+CREATE TRIGGER prevent_deleting_on_table_accounttype
+BEFORE DELETE ON AccountType
+FOR EACH ROW
+EXECUTE FUNCTION prevent_delete_accounttype();
 
--- CREATE TRIGGER prevent_deleting_on_table_accounttype ON AccountType INSTEAD OF DELETE AS RAISERROR ('Cannot delete from table AccountType', 16, 2);
--- CREATE TRIGGER prevent_deleting_on_table_accounts ON Account INSTEAD OF DELETE AS RAISERROR ('Cannot delete from table Account', 16, 3);
--- CREATE TRIGGER prevent_deleting_on_table_item ON Item INSTEAD OF DELETE AS RAISERROR ('Cannot delete from table Item', 16, 4);
--- CREATE TRIGGER prevent_deleting_on_table_paymentorder ON PaymentOrder INSTEAD OF DELETE AS RAISERROR ('Cannot delete from table PaymentOrder', 16, 5);
--- CREATE TRIGGER prevent_deleting_on_table_orderentry ON OrderEntry INSTEAD OF DELETE AS RAISERROR ('Cannot delete from table OrderEntry', 16, 6);
--- CREATE TRIGGER prevent_deleting_on_table_payment ON Payment INSTEAD OF DELETE AS RAISERROR ('Cannot delete from table Payment', 16, 7);
--- CREATE TRIGGER prevent_deleting_on_table_settings ON Settings INSTEAD OF DELETE AS RAISERROR ('Cannot delete from table Settings', 16, 8);
--- CREATE TRIGGER prevent_deleting_on_table_dbsettings ON DBsettings INSTEAD OF DELETE AS RAISERROR ('Cannot drop table DBsettings', 16, 19);
--- CREATE TRIGGER prevent_deleting_on_table_pdf ON PDF INSTEAD OF DELETE AS RAISERROR ('Cannot delete from table PDF', 16, 9);
--- CREATE TRIGGER prevent_deleting_on_table_pdfdownloads ON PDFDownloads INSTEAD OF DELETE AS RAISERROR ('Cannot delete from table PDFDownloads', 16, 10);
+CREATE OR REPLACE FUNCTION prevent_delete_accounts()
+RETURNS trigger AS $$
+BEGIN
+    RAISE EXCEPTION 'Cannot delete from table Account';
+    -- This will prevent the delete operation
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
 
--- CREATE TRIGGER prevent_dropping_on_table_vendor ON Vendor INSTEAD OF DROP AS RAISERROR ('Cannot drop table Vendor', 16, 11);
--- CREATE TRIGGER prevent_dropping_on_table_accounttype ON AccountType INSTEAD OF DROP AS RAISERROR ('Cannot drop table AccountType', 16, 12);
--- CREATE TRIGGER prevent_dropping_on_table_accounts ON Account INSTEAD OF DROP AS RAISERROR ('Cannot drop table Account', 16, 13);
--- CREATE TRIGGER prevent_dropping_on_table_item ON Item INSTEAD OF DROP AS RAISERROR ('Cannot drop table Item', 16, 14);
--- CREATE TRIGGER prevent_dropping_on_table_paymentorder ON PaymentOrder INSTEAD OF DROP AS RAISERROR ('Cannot drop table PaymentOrder', 16, 15);
--- CREATE TRIGGER prevent_dropping_on_table_orderentry ON OrderEntry INSTEAD OF DROP AS RAISERROR ('Cannot drop table OrderEntry', 16, 16);
--- CREATE TRIGGER prevent_dropping_on_table_payment ON Payment INSTEAD OF DROP AS RAISERROR ('Cannot drop table Payment', 16, 17);
--- CREATE TRIGGER prevent_dropping_on_table_settings ON Settings INSTEAD OF DROP AS RAISERROR ('Cannot drop table Settings', 16, 18);
--- CREATE TRIGGER prevent_dropping_on_table_dbsettings ON DBsettings INSTEAD OF DROP AS RAISERROR ('Cannot drop table DBsettings', 16, 19);
--- CREATE TRIGGER prevent_dropping_on_table_pdf ON PDF INSTEAD OF DROP AS RAISERROR ('Cannot drop table PDF', 16, 19);
--- CREATE TRIGGER prevent_dropping_on_table_pdfdownloads ON PDFDownloads INSTEAD OF DROP AS RAISERROR ('Cannot drop table PDFDownloads', 16, 20);
+CREATE TRIGGER prevent_deleting_on_table_accounts
+BEFORE DELETE ON Account
+FOR EACH ROW
+EXECUTE FUNCTION prevent_delete_accounts();
 
+CREATE OR REPLACE FUNCTION prevent_delete_item()
+RETURNS trigger AS $$
+BEGIN
+    RAISE EXCEPTION 'Cannot delete from table Item';
+    -- This will prevent the delete operation
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER prevent_deleting_on_table_item
+BEFORE DELETE ON Item
+FOR EACH ROW
+EXECUTE FUNCTION prevent_delete_item();
+
+CREATE OR REPLACE FUNCTION prevent_delete_paymentorder()
+RETURNS trigger AS $$
+BEGIN
+    RAISE EXCEPTION 'Cannot delete from table PaymentOrder';
+    -- This will prevent the delete operation
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER prevent_deleting_on_table_paymentorder
+BEFORE DELETE ON PaymentOrder
+FOR EACH ROW
+EXECUTE FUNCTION prevent_delete_paymentorder();
+
+CREATE OR REPLACE FUNCTION prevent_delete_orderentry()
+RETURNS trigger AS $$
+BEGIN
+    RAISE EXCEPTION 'Cannot delete from table OrderEntry';
+    -- This will prevent the delete operation
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER prevent_deleting_on_table_orderentry
+BEFORE DELETE ON OrderEntry
+FOR EACH ROW
+EXECUTE FUNCTION prevent_delete_orderentry();
+
+CREATE OR REPLACE FUNCTION prevent_delete_payment()
+RETURNS trigger AS $$
+BEGIN
+    RAISE EXCEPTION 'Cannot delete from table Payment';
+    -- This will prevent the delete operation
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER prevent_deleting_on_table_payment
+BEFORE DELETE ON Payment
+FOR EACH ROW
+EXECUTE FUNCTION prevent_delete_payment();
+
+CREATE OR REPLACE FUNCTION prevent_delete_settings()
+RETURNS trigger AS $$
+BEGIN
+    RAISE EXCEPTION 'Cannot delete from table Settings';
+    -- This will prevent the delete operation
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER prevent_deleting_on_table_settings
+BEFORE DELETE ON Settings
+FOR EACH ROW
+EXECUTE FUNCTION prevent_delete_settings();
+
+CREATE OR REPLACE FUNCTION prevent_delete_dbsettings()
+RETURNS trigger AS $$
+BEGIN
+    RAISE EXCEPTION 'Cannot delete from table DBsettings';
+    -- This will prevent the delete operation
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER prevent_deleting_on_table_dbsettings
+BEFORE DELETE ON DBsettings
+FOR EACH ROW
+EXECUTE FUNCTION prevent_delete_dbsettings();
+
+CREATE OR REPLACE FUNCTION prevent_delete_pdf()
+RETURNS trigger AS $$
+BEGIN
+    RAISE EXCEPTION 'Cannot delete from table PDF';
+    -- This will prevent the delete operation
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER prevent_deleting_on_table_pdf
+BEFORE DELETE ON PDF
+FOR EACH ROW
+EXECUTE FUNCTION prevent_delete_pdf();
+
+CREATE OR REPLACE FUNCTION prevent_delete_pdfdownloads()
+RETURNS trigger AS $$
+BEGIN
+    RAISE EXCEPTION 'Cannot delete from table PDFDownloads';
+    -- This will prevent the delete operation
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER prevent_deleting_on_table_pdfdownloads
+BEFORE DELETE ON PDFDownloads
+FOR EACH ROW
+EXECUTE FUNCTION prevent_delete_pdfdownloads();
 
 ---- create above / drop below ----
 
 
-
-
-DROP TRIGGER prevent_deleting_on_table_vendor;
--- DROP TRIGGER prevent_deleting_on_table_accounttype;
--- DROP TRIGGER prevent_deleting_on_table_accounts;
--- DROP TRIGGER prevent_deleting_on_table_item;
--- DROP TRIGGER prevent_deleting_on_table_paymentorder;
--- DROP TRIGGER prevent_deleting_on_table_orderentry;
--- DROP TRIGGER prevent_deleting_on_table_payment;
--- DROP TRIGGER prevent_deleting_on_table_settings;
--- DROP TRIGGER prevent_deleting_on_table_dbsettings;
--- DROP TRIGGER prevent_deleting_on_table_pdf;
--- DROP TRIGGER prevent_deleting_on_table_pdfdownloads;
-
--- DROP TRIGGER prevent_dropping_on_table_vendor ON Vendor;
--- DROP TRIGGER prevent_dropping_on_table_accounttype ON AccountType;
--- DROP TRIGGER prevent_dropping_on_table_accounts ON Account;
--- DROP TRIGGER prevent_dropping_on_table_item ON Item;
--- DROP TRIGGER prevent_dropping_on_table_paymentorder ON PaymentOrder;
--- DROP TRIGGER prevent_dropping_on_table_orderentry ON OrderEntry;
--- DROP TRIGGER prevent_dropping_on_table_payment ON Payment;
--- DROP TRIGGER prevent_dropping_on_table_settings ON Settings;
--- DROP TRIGGER prevent_dropping_on_table_dbsettings ON DBsettings;
--- DROP TRIGGER prevent_dropping_on_table_pdf ON PDF;
--- DROP TRIGGER prevent_dropping_on_table_pdfdownloads ON PDFDownloads;
-
-
+DROP TRIGGER prevent_deleting_on_table_vendor ON Vendor;
+DROP TRIGGER prevent_deleting_on_table_accounttype ON AccountType;
+DROP TRIGGER prevent_deleting_on_table_accounts ON Account;
+DROP TRIGGER prevent_deleting_on_table_item ON Item;
+DROP TRIGGER prevent_deleting_on_table_paymentorder ON PaymentOrder;
+DROP TRIGGER prevent_deleting_on_table_orderentry ON OrderEntry;
+DROP TRIGGER prevent_deleting_on_table_payment ON Payment;
+DROP TRIGGER prevent_deleting_on_table_settings ON Settings;
+DROP TRIGGER prevent_deleting_on_table_dbsettings ON DBsettings;
+DROP TRIGGER prevent_deleting_on_table_pdf ON PDF;
+DROP TRIGGER prevent_deleting_on_table_pdfdownloads ON PDFDownloads;
 
 -- Write your migrate down statements here. If this migration is irreversible
 -- Then delete the separator line above.
