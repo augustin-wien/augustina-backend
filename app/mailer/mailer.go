dd3ff506b37f4bdb4f391e885fb491a5daf56ce7
Aaron <aaron@nanu-c.org>
2025-11-13 22:39:57 +0100

    fix(mailer): support Office365 STARTTLS and correct auth host
    - Use STARTTLS on non-SSL connections (port 587) when the server advertises STARTTLS (Office365 compatibility).
    - Use TLS-on-connect for SMTPSsl=true (port 465).
    - Ensure smtp.PlainAuth uses server name (no port) and avoid InsecureSkipVerify.
    - Improve SendEmail flow to use smtp.Client for STARTTLS and TLS connections.

--- app/mailer/mailer.go
+++ app/mailer/mailer.go
@@ -18,10 +18,9 @@ var auth smtp.Auth
 
 func Init() {
 	log.Infoln("Initializing mailer")
-	host := config.Config.SMTPServer + ":" + config.Config.SMTPPort
-	if !config.Config.SMTPSsl {
-		host = config.Config.SMTPServer
-	}
+	// smtp.PlainAuth requires the server name (without port)
+	host := config.Config.SMTPServer
+	auth = smtp.PlainAuth("", config.Config.SMTPUsername, config.Config.SMTPPassword, host)
 
 	auth = smtp.PlainAuth("", config.Config.SMTPUsername, config.Config.SMTPPassword, host)
 }
@@ -57,83 +56,123 @@ func (r *EmailRequest) SendEmail() (bool, error) {
 	subject := "Subject: " + r.subject + "!\n"
 	msg := []byte(from + subject + mime + "\n" + r.body)
 
+	addr := config.Config.SMTPServer + ":" + config.Config.SMTPPort
+
 	if !config.Config.SMTPSsl {
-		log.Info("Sending email to ", r.to, " with subject ", r.subject)
-		addr := config.Config.SMTPServer + ":" + config.Config.SMTPPort
-		if err := smtp.SendMail(addr, auth, config.Config.SMTPUsername, r.to, msg); err != nil {
-			log.Error("SendEmail: noSSL: Error sending email ", err)
-			return false, err
-		}
-		return true, nil
-	} else {
-		log.Info("Sending ssl email to ", r.to, " with subject ", r.subject)
-
-		// TLS config
-		tlsconfig := &tls.Config{
-			InsecureSkipVerify: true,
-			ServerName:         config.Config.SMTPServer,
-		}
-		host := config.Config.SMTPServer + ":" + config.Config.SMTPPort
-
-		// Here is the key, you need to call tls.Dial instead of smtp.Dial
-		// for smtp servers running on 465 that require an ssl connection
-		// from the very beginning (no starttls)
-		conn, err := tls.Dial("tcp", host, tlsconfig)
+		// Try to use STARTTLS if the server supports it (Office365 requires STARTTLS on port 587)
+		log.Info("Sending (STARTTLS) email to ", r.to, " with subject ", r.subject)
+		c, err := smtp.Dial(addr)
 		if err != nil {
-			log.Error("SendEmail: failed tls dial", err)
+			log.Error("SendEmail: dial error", err)
 			return false, err
 		}
+		defer c.Close()
 
-		c, err := smtp.NewClient(conn, host)
-		if err != nil {
-			log.Error("SendMail: Ssl: create smtp client", err)
-			return false, err
-
+		// Upgrade to TLS if supported
+		if ok, _ := c.Extension("STARTTLS"); ok {
+			tlsconfig := &tls.Config{
+				ServerName: config.Config.SMTPServer,
+			}
+			if err = c.StartTLS(tlsconfig); err != nil {
+				log.Error("SendEmail: STARTTLS failed", err)
+				return false, err
+			}
 		}
 
 		// Auth
-		if err = c.Auth(auth); err != nil {
-			log.Error("SendMail: Ssl:", err)
-			return false, err
+		if auth != nil {
+			if err = c.Auth(auth); err != nil {
+				log.Error("SendEmail: auth failed", err)
+				return false, err
+			}
 		}
 
-		// To && From
+		// From
 		if err = c.Mail(config.Config.SMTPSenderAddress); err != nil {
-			log.Error("SendMail: Ssl: create mail: ", err)
+			log.Error("SendEmail: set mail failed", err)
 			return false, err
 		}
 
-		if err = c.Rcpt(r.to[0]); err != nil {
-			log.Error("SendMail: Ssl: set recipient:", err)
-			return false, err
+		// To
+		for _, rcpt := range r.to {
+			if err = c.Rcpt(rcpt); err != nil {
+				log.Error("SendEmail: rcpt failed", err)
+				return false, err
+			}
 		}
 
 		// Data
 		w, err := c.Data()
 		if err != nil {
-			log.Error("SendMail: Ssl: data", err)
+			log.Error("SendEmail: data failed", err)
 			return false, err
 		}
-
-		_, err = w.Write([]byte(msg))
-		if err != nil {
-			log.Error("SendMail: Ssl: send message", err)
+		if _, err = w.Write([]byte(msg)); err != nil {
+			log.Error("SendEmail: write failed", err)
 			return false, err
 		}
-
-		err = w.Close()
-		if err != nil {
-			log.Error("SendMail: Ssl: close connection", err)
+		if err = w.Close(); err != nil {
+			log.Error("SendEmail: close failed", err)
 			return false, err
 		}
-
-		err = c.Quit()
-		if err != nil {
-			log.Error("SendMail: Ssl: quit", err)
+		if err = c.Quit(); err != nil {
+			log.Error("SendEmail: quit failed", err)
 			return false, err
 		}
 		return true, nil
 	}
+
+	// SMTPSsl == true -> connect with TLS from the start (port 465)
+	log.Info("Sending SSL email to ", r.to, " with subject ", r.subject)
+	tlsconfig := &tls.Config{
+		ServerName: config.Config.SMTPServer,
+	}
+	conn, err := tls.Dial("tcp", addr, tlsconfig)
+	if err != nil {
+		log.Error("SendEmail: TLS dial failed", err)
+		return false, err
+	}
+	c, err := smtp.NewClient(conn, config.Config.SMTPServer)
+	if err != nil {
+		log.Error("SendEmail: create smtp client failed", err)
+		return false, err
+	}
+	defer c.Close()
+
+	if auth != nil {
+		if err = c.Auth(auth); err != nil {
+			log.Error("SendEmail: auth failed", err)
+			return false, err
+		}
+	}
+	if err = c.Mail(config.Config.SMTPSenderAddress); err != nil {
+		log.Error("SendEmail: set mail failed", err)
+		return false, err
+	}
+	for _, rcpt := range r.to {
+		if err = c.Rcpt(rcpt); err != nil {
+			log.Error("SendEmail: rcpt failed", err)
+			return false, err
+		}
+	}
+	w, err := c.Data()
+	if err != nil {
+		log.Error("SendEmail: data failed", err)
+		return false, err
+	}
+	if _, err = w.Write([]byte(msg)); err != nil {
+		log.Error("SendEmail: write failed", err)
+		return false, err
+	}
+	if err = w.Close(); err != nil {
+		log.Error("SendEmail: close failed", err)
+		return false, err
+	}
+	if err = c.Quit(); err != nil {
+		log.Error("SendEmail: quit failed", err)
+		return false, err
+	}
+	return true, nil
 
 }
 
